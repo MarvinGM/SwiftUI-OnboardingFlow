2b1bf5e0ed34d61932b6624837bb60336577fcb7
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 14:24:44 +0800

    Initial Commit

---
+++ SwiftUI OnboardingFlow/SwiftUI_OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OnboardingFlowApp.swift
+//  SwiftUI OnboardingFlow
+//
+//  Created by Amben on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
